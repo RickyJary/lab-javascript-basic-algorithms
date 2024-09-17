c03a1dbe8f84fcdab70fea1e5fc27a7ea2949a8f
RickyJary <ricardogarciajarillo0209@gmail.com>
2024-09-17 20:35:51 +0200

    Iteration 3 and Bonus done!

--- index.js
+++ index.js
@@ -19,7 +19,56 @@ if (hacker1.length > hacker2.length) {
 }
 
 // Iteration 3: Loops
+//3.1
+let upperCaseName = "";
 
 for(let i=0; i < hacker1.length; i++) {
-    console.log(hacker1[i].toUpperCase());
+    upperCaseName += hacker1[i].toUpperCase() + " ";
 } 
+console.log(upperCaseName)
+
+//3.2
+
+let reversedName = "";
+
+for(let j=hacker2.length - 1; j>=0; j--) {
+    reversedName += hacker2[j];
+}
+console.log(reversedName)
+
+3.3
+
+if(hacker1.charAt(0)<hacker2.charAt(0)){
+    console.log("The driver's name goes first.");
+} else if(hacker1.charAt(0)>hacker2.charAt(0)){
+    console.log("Yo, the navigator goes first, definitely.");
+} else {
+    console.log("What?! You both have the same name?")
+};
+
+// Bonus 1
+
+let longText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam et eleifend arcu, eu commodo diam. Phasellus sed nulla eu leo maximus aliquam. Integer risus ipsum, eleifend sit amet arcu et, cursus viverra ex. Cras bibendum euismod ligula, eu sagittis justo faucibus sit amet. Integer fringilla magna a augue pellentesque, maximus porta felis commodo. Morbi tempus nec velit in congue. Mauris blandit metus eget accumsan pharetra. Donec eu scelerisque nibh. Mauris imperdiet libero quis urna rhoncus, vitae congue lectus ultricies. Mauris mattis nisi quis ex consectetur, at vulputate justo commodo. Etiam nec est ex. In odio nisi, feugiat sit amet purus vitae, dapibus faucibus velit. Etiam sed diam dui. Curabitur pharetra sem id molestie condimentum. Vestibulum sit amet molestie nibh. Sed et risus ac ligula euismod cursus. Proin id mi sed dolor luctus finibus sed a libero. Sed egestas tortor ac ante tincidunt, quis pellentesque nisl sollicitudin. Maecenas et sem malesuada, pulvinar quam eu, feugiat massa. Quisque pharetra nisi id commodo egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas, est hendrerit suscipit pretium, neque augue viverra sapien, non viverra turpis augue mattis sem. Suspendisse ut condimentum lacus. Suspendisse in nulla ac diam accumsan laoreet. Vestibulum in neque elit. Praesent non auctor arcu. Nullam tristique pulvinar ligula. Morbi sit amet aliquet est, eget tincidunt sem. Suspendisse in mauris gravida, accumsan arcu a, lobortis dolor. In neque quam, vulputate eu leo eget, pharetra laoreet felis. In non elit semper, efficitur eros eget, condimentum felis. Vivamus quis leo ut lorem vestibulum volutpat. In semper porta maximus. Phasellus nec dignissim libero. Ut sit amet odio id nunc cursus placerat ac ac orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam nec lorem mattis, euismod metus ut, sagittis elit. Curabitur feugiat consectetur dolor, a scelerisque elit maximus sit amet. Vivamus eget justo lacus. Donec pharetra convallis luctus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed viverra, magna et feugiat lacinia, velit risus elementum sapien, eget maximus leo urna a est."
+
+
+
+let countWords = 0;
+
+for(let k=0; k<longText.length;k++) {
+    if (longText[k] === " ") {
+        countWords++;
+    } 
+}
+
+console.log(countWords);
+
+let countEt = 0;
+
+for(let l=0; l<longText.length;l++) {
+    let doubleChar = longText[l] + longText[l+1];
+    if (doubleChar === "et") {
+        countEt++;
+    } 
+}
+
+console.log(countEt);
